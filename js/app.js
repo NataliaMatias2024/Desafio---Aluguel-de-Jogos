2970136afadae31ff3135dfb6c280e69c5421ddc
Natalia Matias <dontreplay@github.com>
2024-02-13 22:44:53 -0300

    Remover log e linhas repetidas de testes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,9 @@
 function alterarStatus(id){
-    reconhecerClick(id);
-    conferirDados(id);
+    let click = document.getElementById(`game-${id}`);
     mudarStatusImagem(id);
     mudarStatusClasse(id);
    }
-   
-   function reconhecerClick(id) {
-       let click = document.getElementById(`game-${id}`);
-       console.log(id);
-   }
-   
-   function conferirDados(id){
-       let click = document.getElementById(`game-${id}`);
-       let classeImagem = click.querySelector(".dashboard__item__img");
-       let classeStatus = click.querySelector(".dashboard__item__button");
-       let nomeJogo = click.querySelector(".dashboard__item__name")
-       console.log(classeImagem/*.textContent*/);// essa tag é da imagem do jogo, por isso n tm "nome"dentro dela
-       console.log(classeStatus.textContent); //textContent vai imprimir o nome dentro da tag e n a tag td. 
-       console.log(nomeJogo.textContent); //textContent vai imprimir o nome dentro da tag e n a tag td. 
-   }
-   
+
    function mudarStatusImagem(id){
        let click = document.getElementById(`game-${id}`);
        let classeImagem = click.querySelector(".dashboard__item__img");
