0f9bd20703e98b5693b0cd5c4ddcccfc22209918
Natalia Matias <dontreplay@github.com>
2024-02-15 15:39:43 -0300

    Criar contador de jogos devolvidos/alugados ao terminar o dia

--- js/app.js
+++ js/app.js
@@ -13,10 +13,29 @@ function alterarStatus(id){
            //a condiçao confirme cria uma caixa de dialogo com OK e cancelar, OK semrpe retorna true e cancelar false
            classeImagem.classList.remove("dashboard__item__img--rented");
            classeBotao.classList.remove("dashboard__item__button--return");
-           classeBotao.innerHTML = `Alugar`; 
+           classeBotao.innerHTML = `Alugar`;
+           avaliacaoDevolvidos(id); 
+           
        } else {
            classeImagem.classList.add("dashboard__item__img--rented");
            classeBotao.classList.add("dashboard__item__button--return");
            classeBotao.innerHTML = `Devolver`;
+           avaliacaoAlugados(id);
        }
    }
+
+   //Crie uma função para imprimir no console quantos jogos foram alugados.
+   let jogosAlugados = 0;
+   let jogosDevolvidos = 0;
+
+    function avaliacaoAlugados(id) {
+        let click = document.getElementById(`game-${id}`); 
+        jogosAlugados++;
+        console.log (`Hoje foram alugados ${jogosAlugados} jogos.`); 
+        }
+
+    function avaliacaoDevolvidos(id) {
+             let click = document.getElementById(`game-${id}`); 
+             jogosDevolvidos++;
+             console.log(`Hoje foram devolvidos ${jogosDevolvidos} jogos.`);            
+             }
